b9c32cb3853fa62aff500466d339ce58f1e00d1a
Ben Nemec <cybertron@nemebean.com>
2012-11-16 23:23:57 -0600

    Add license information

--- nemebean/mythnimal/MythDBObjects.py
+++ nemebean/mythnimal/MythDBObjects.py
@@ -1,3 +1,21 @@
+# @Begin License@
+# This file is part of Mythnimal.
+#
+# Mythnimal is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mythnimal is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mythnimal.  If not, see <http:#www.gnu.org/licenses/>.
+#
+# Copyright 2012 Ben Nemec
+# @End License@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 Base = declarative_base()
